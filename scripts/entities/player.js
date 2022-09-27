78a1dfffdee033b04c290b85761a28a07aa82e95
sase <bs.saase@gmail.com>
2022-09-26 21:13:55 -0400

    created player class

---
+++ scripts/entities/player.js
@@ -0,0 +1,49 @@
+import Entity from './entityClass.js';
+
+class Player extends Entity {
+  constructor(x, y, hp, pushOnFrame, speed=5) {
+    super(x, y, hp, 'player', pushOnFrame);
+    this.speed = speed;
+    this.prevSpeed = speed;
+    this.inShift = false;
+    this.dir = [];
+
+    this.initEvents();
+  }
+
+  initEvents() {
+    const moveKeys = {w:1,a:1,s:1,d:1};
+    document.addEventListener("keydown", (e) => {
+      let key = e.key.toLowerCase();
+      const isMoveKey = moveKeys[key];
+      const notInDir = this.dir.indexOf(key) === -1;
+
+      if (key === 'shift') this.inShift = true;
+      else if (isMoveKey && notInDir) this.dir.unshift(key);
+    });
+
+    document.addEventListener("keyup", (e) => {
+      let key = e.key.toLowerCase();
+      const isMoveKey = moveKeys[key];
+      const indexInDir = this.dir.indexOf(key);
+      const inDir = indexInDir !== -1;
+
+      if (key === 'shift') this.inShift = false;
+      else if (isMoveKey && inDir) this.dir.splice(indexInDir, 1);
+    });
+
+    this.pushOnFrame(this.checkMove);
+  }
+
+  checkMove() {
+    if (this.dir.length === 0) return;
+    let dir = this.dir[0];
+
+    if (dir === 'w') this.y -= this.speed;
+    else if (dir === 's') this.y += this.speed;
+    else if (dir === 'a') this.x -= this.speed;
+    else if (dir === 'd') this.x += this.speed;
+  }
+};
+
+export default Player;
