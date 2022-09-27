b356006ec143b3a7606c8f4bad1a4d296809d354
sase <bs.saase@gmail.com>
2022-09-26 21:00:43 -0400

    created entity class

---
+++ scripts/entity/entityClass.js
@@ -0,0 +1,21 @@
+import $ from '../helper/dom.js';
+const emptyFunc = () => {};
+
+class Entity {
+  constructor(x=0, y=0, hp=100, classes=[], pushOnFrame=emptyFunc, parent=$.get('#app')) {
+    this.parent = parent;
+    this.x = x;
+    this.y = y;
+    this.health = hp;
+    this.pushOnFrame = pushOnFrame;
+
+    this.build(classes);
+  }
+
+  build(classes) {
+    this.node = $.make(classes);
+    $.append(this.node, this.parent);
+  }
+};
+
+export default Entity;
---
+++ scripts/helper/dom.js
@@ -0,0 +1,44 @@
+const get = (query) => {
+  if (query[0] === '.' || query[0] === '#') {
+    return document.querySelector(query);
+  } else {
+    return document.querySelector(`.${query}`);
+  }
+};
+
+const append = (child, parent) => {
+  try {
+    parent.appendChild(child);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+const make = (classes=[], type='div', id=null) => {
+  let node = document.createElement(type);
+  (Array.isArray(classes)? classes : [classes]).forEach(className => {
+    node.classList.add(className);
+  });
+  if (id) node.id = id;
+  return node;
+};
+
+const css = (element, cssPropArr) => {
+  cssPropArr.forEach(propArr => {
+    const prop = propArr[0];
+    const val = propArr[1];
+    const unit = propArr[2] || "";
+
+    try {
+      element.style[prop] = `${val}${unit}`;
+      return true;
+    } catch {
+      return false;
+    }
+  });
+};
+
+export default {
+  get, make, append, css
+};
