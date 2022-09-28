e2a514cddff186fbe23d4c060f6a8bbc7401bbb0
sase <bs.saase@gmail.com>
2022-09-28 19:41:08 -0400

    created basic hotbar inventory

--- scripts/entities/player/player.js
+++ scripts/entities/player/player.js
@@ -1,5 +1,6 @@
 import Entity from '../entityClass.js';
 import Inventory from './inventory.js';
+import $ from '../../helper/dom.js';
 
 class Player extends Entity {
   constructor(x, y, hp, pushOnFrame, speed=3.5) {
@@ -8,6 +9,7 @@ class Player extends Entity {
     this.inShift = false;
     this.speed = speed;
     this.prevSpeed = speed;
+    this.currentSlot = null;
     
     // Meta
     this.crouchSpeedMultiplier = 0.365;
@@ -15,12 +17,14 @@ class Player extends Entity {
     this.inventory = new Inventory(20);
 
     this.initEvents();
+    this.scrollEvents();
   }
 
   initEvents() {
     const moveKeys = {w:1,a:1,s:1,d:1};
     document.addEventListener("keydown", (e) => {
-      let key = e.key.toLowerCase();
+      const key = e.key.toLowerCase();
+      const keyInNum = parseInt(key);
       const isMoveKey = moveKeys[key];
       const notInDir = this.dir.indexOf(key) === -1;
 
@@ -31,6 +35,8 @@ class Player extends Entity {
         this.speed = this.speed * this.crouchSpeedMultiplier;
       }
       else if (isMoveKey && notInDir) this.dir.unshift(key);
+      
+      if (!isNaN(keyInNum) && keyInNum < 5 && keyInNum > 0) this.switchSlots(keyInNum);
     });
 
     document.addEventListener("keyup", (e) => {
@@ -57,6 +63,32 @@ class Player extends Entity {
     else if (dir === 's') this.y += this.speed;
     else if (dir === 'a') this.x -= this.speed;
     else if (dir === 'd') this.x += this.speed;
+  }
+
+  switchSlots(slotNumber) {
+    if (slotNumber === 5) slotNumber = 1;
+    else if  (slotNumber === 0) slotNumber = 4;
+
+    const oldSlot = $.get(`hotbar-slot-${this.currentSlot}`);
+    const newSlot = $.get(`hotbar-slot-${slotNumber}`);
+
+    if (this.currentSlot !== null) {
+      oldSlot.style.background = "url(/public/assets/ui/inventory-slot.png)";
+      oldSlot.style.backgroundSize = "cover";
+    }
+
+    this.currentSlot = slotNumber;
+    newSlot.style.background = "url(/public/assets/ui/selected-inventory-slot.png)";
+    newSlot.style.backgroundSize = "cover";
+  }
+
+  scrollEvents() {
+    document.addEventListener("wheel", (e) => {
+      const isUp = e.deltaY < 0;
+
+      if (isUp) this.switchSlots(this.currentSlot - 1 || 0);
+      else this.switchSlots(this.currentSlot + 1 || 4);
+    });
   }
 };
 
