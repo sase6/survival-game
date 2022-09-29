6e8b261f868c27c0a3cdfaa061bd9f6fded78894
sase <bs.saase@gmail.com>
2022-09-28 21:20:24 -0400

    turned per frame into an object for easy deletions

--- scripts/index.js
+++ scripts/index.js
@@ -3,24 +3,37 @@ import spawn from './entities/spawn.js';
 
 class World {
   constructor() {
-    this.perFrame = [];
-    this.pushOnFrame = (func) => this.perFrame.push(func);
+    this.perFrame = {};
+    this.entityCount = 0;
+
+    // Functions
+    this.incrementEntity = () => this.entityCount++;
+    this.pushOnFrame = (id, arrOfFuncs) => {
+      let arrOfFuncs = Array.isArray(arrOfFuncs)? arrOfFuncs : [arrOfFuncs];
+      if (this.perFrame[id] !== undefined) this.perFrame[i] === [...this.perFrame[id], ...arrOfFuncs];
+      else this.perFrame[i] = arrOfFuncs;
+    };
+
+    // Global Entities
     this.player = new Player(0, 0, 100, this.pushOnFrame);
 
-    // Spawns
-    spawn.waterBodies(this.pushOnFrame, this.player);
-    spawn.stones(1, this.pushOnFrame, this.player);
-    spawn.grasses(15, this.pushOnFrame, this.player);
-    spawn.deers(12.5, this.pushOnFrame, this.player);
-    spawn.trees(40, 20, this.pushOnFrame, this.player);
+    // Spawn Entities
+    // spawn.waterBodies(this.pushOnFrame, this.player);
+    // spawn.stones(1, this.pushOnFrame, this.player);
+    // spawn.grasses(15, this.pushOnFrame, this.player);
+    // spawn.deers(12.5, this.pushOnFrame, this.player);
+    // spawn.trees(40, 20, this.pushOnFrame, this.player);
 
     this.loop();
   }
 
   loop() {
-    for (let i = 0; i < this.perFrame.length; i++) {
-      const func = this.perFrame[i];
-      func();
+    for (let entityId in this.perFrame) {
+      const funcArr = this.perFrame[entityId];
+      for (let i = 0; i < funcArr.length; i++) {
+        const func = funcArr[i];
+        func();
+      }
     }
 
     window.requestAnimationFrame(() => this.loop());
